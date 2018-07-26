4d761fe861dc8c3d22c8e1e235881eecea3a00df
GLOBAL\I343436 <jason.noh@sap.com>
2018-07-26 13:15:37 +1000

    initial template, sass compatable

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return ( 
+      <div>
+        <div className="union">
+        hello world
+        </div>
+        <div className="union2">
+        Hello world
+        </div>
+      </div>
+    );
+  }
+}
+/*
+.welcome {
+  color:red;
+}
+*/
+export default App;
